ff68c1f2a8b1f84207b12f94f810195dc08b75f9
Oscar Stenström <oscar.stenstrom2001@gmail.com>
2026-09-07 14:53:36 +0200

    Inital test written

---
+++ WordLetterCounter.Tests/TextAnalyserTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace WordLetterCounter.Tests
+{
+    public class TextAnalyserTests
+    {
+        private readonly TextAnalyzer _textAnalyzer = new TextAnalyzer();
+
+
+        [Fact]
+        public void Count_LettersFromSentence_Returns5()
+        {
+            // Arrange
+
+            string test = "Jag gillar glass o öl";
+            int expected = 5;
+            //Act
+            var result = _textAnalyzer.CountLetters(test);
+            // Assert
+
+            Assert.Equal(25, result);
+        }
+    }
+}
