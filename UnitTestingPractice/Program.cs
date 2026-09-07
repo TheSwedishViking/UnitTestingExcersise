3ce37a9f418a7f363be5829606da3b3a4994847b
Robin Bertling <robinbertling10@gmail.com>
2026-09-07 14:03:56 +0200

    vs

---
+++ UnitTestingPractice/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace UnitTestingPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Oscar igen!");
+        }
+    }
+}
